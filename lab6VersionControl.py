ee8ef3e1ed505bec72a8e4d1001e91c328420eeb
Lreid <185863782+Loreyn626@users.noreply.github.com>
2024-10-23 18:22:00 -0400

    updated spacing

--- lab6VersionControl.py
+++ lab6VersionControl.py
@@ -9,7 +9,6 @@ def pass_to_encode(original):
         if i > 9:
             i -= 10
         res += str(i)
-
     return res
 
 def decode(encoded_pass):
@@ -25,12 +24,9 @@ def decode(encoded_pass):
         else:
             new_digit = (int(digit) - 3)
         decoded_pass += str(new_digit)
-
     return decoded_pass
 
-
 def main():
-
 
     while True:
         print("Menu")
@@ -53,7 +49,6 @@ def main():
 
         if menu_option == 3:
             break
-
 
 if __name__ == "__main__":
     main()
